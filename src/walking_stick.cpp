2703ba590e288130708ae5c037eab7d9a3a11527
Ranuka Perera <random@sawrc.com>
2019-04-14 01:08:40 +0530

    * Start adding audio

--- src/walking_stick.cpp
+++ src/walking_stick.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <NewPing.h>
+#include <SoftwareSerial.h>
 #include <DFMiniMp3.h>
 
 #include "EasingLibrary.h"
@@ -14,12 +15,77 @@
 #define LOWER_SENSOR_COM_PIN 12
 #define LOWER_SENSOR_POWER_PIN 11  // Pin to turn on power to the sensor
 #define LOWER_SENSOR_MAX_DISTANCE 60 // In centimetres
+#define AUDIO_TX 10
 
 // ------------------------------------------------ENUMS----------------------------------------------------------------
+class Mp3Notify
+{
+public:
+  static void OnError(uint16_t errorCode)
+  {
+    // see DfMp3_Error for code meaning
+    Serial.println();
+    Serial.print("Com Error ");
+    Serial.println(errorCode);
+  }
 
+  static void OnPlayFinished(uint16_t globalTrack)
+  {
+    Serial.println();
+    Serial.print("Play finished for #");
+    Serial.println(globalTrack);   
+  }
+
+  static void OnCardOnline(uint16_t code)
+  {
+    Serial.println();
+    Serial.print("Card online ");
+    Serial.println(code);
+  }
+
+  static void OnUsbOnline(uint16_t code)
+  {
+    Serial.println();
+    Serial.print("USB Disk online ");
+    Serial.println(code);
+  }
+
+  static void OnCardInserted(uint16_t code)
+  {
+    Serial.println();
+    Serial.print("Card inserted ");
+    Serial.println(code);
+  }
+
+  static void OnUsbInserted(uint16_t code)
+  {
+    Serial.println();
+    Serial.print("USB Disk inserted ");
+    Serial.println(code); 
+  }
+
+  static void OnCardRemoved(uint16_t code)
+  {
+    Serial.println();
+    Serial.print("Card removed ");
+    Serial.println(code);  
+  }
+
+  static void OnUsbRemoved(uint16_t code)
+  {
+    Serial.println();
+    Serial.print("USB Disk removed ");
+    Serial.println(code);
+  }
+};
 
 // ----------------------------------------------GLOBAL VARIABLES-------------------------------------------------------
 NewPing sonar_l(LOWER_SENSOR_COM_PIN, LOWER_SENSOR_COM_PIN);
+
+SoftwareSerial serial2(AUDIO_TX, 9); // RX, TX
+DFMiniMp3<SoftwareSerial, Mp3Notify> audio(serial2);
+
+unsigned int distance;
 
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -31,16 +97,28 @@ void setup()
     pinMode(LOWER_SENSOR_POWER_PIN, OUTPUT);
     digitalWrite(LOWER_SENSOR_POWER_PIN, LOW);
     Serial.println(F("Sensor 1 ready"));
+
+    audio.begin();
+    audio.setVolume(10);
+    uint16_t count = audio.getTotalTrackCount();
+    Serial.print("files ");
+    Serial.println(count);
 }
 
 void loop()
 {
     unsigned long currentMillis = millis();
     Serial.print("Ping: ");
-    Serial.print(sonar_l.ping_median(10) / US_ROUNDTRIP_CM);
+    distance = sonar_l.ping_median(10) / US_ROUNDTRIP_CM;
+    Serial.print(distance);
     Serial.print("cm, Time Taken: ");
     Serial.print((millis() - currentMillis)/1000.0);
     Serial.println("s");
+
+    if (distance != 0 && distance < 50) {
+        audio.playMp3FolderTrack(1);
+        delay(100);
+    }
     
     // Check distance in sensor 1
     //   if it's close
