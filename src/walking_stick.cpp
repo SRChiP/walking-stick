f56b348d37cb23600a737301002373ee39b50bd0
Ranuka Perera <random@sawrc.com>
2019-04-13 16:15:34 +0530

    - Add initial implementation of the sensor - Add the required libs

--- src/walking_stick.cpp
+++ src/walking_stick.cpp
@@ -1,12 +1,20 @@
 #include <Arduino.h>
+#include <NewPing.h>
+#include <DFMiniMp3.h>
+
 #include "EasingLibrary.h"
 
 
-// --------------------------- SETTINGS ---------------------------------------------------------------------
+// ----------------------------------------------- SETTINGS ------------------------------------------------------------
 
 //Enable or disable DEBUG. Serial is fully disabled if DEBUG is false
 #define DEBUG true // flag to turn on/off debugging
 #define Serial if(DEBUG)Serial
+
+// Settings for the first (lower) sensor
+#define LOWER_SENSOR_TRIGGER_PIN 12
+#define LOWER_SENSOR_ECHO_PIN 11
+#define LOWER_SENSOR_MAX_DISTANCE 60 // In centimetres
 
 // ------------------------------------------------ENUMS----------------------------------------------------------------
 typedef enum {
@@ -17,7 +25,7 @@ typedef enum {
 } STATE;
 
 // ----------------------------------------------GLOBAL VARIABLES-------------------------------------------------------
-
+NewPing sonar_l(LOWER_SENSOR_TRIGGER_PIN, LOWER_SENSOR_TRIGGER_PIN);
 
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -25,12 +33,26 @@ void setup()
 {
     // delay(800);
     Serial.begin(9600);
-    Serial.println(F("Booting stick..."));
-    
+    Serial.println(F("Booting stick..."));    
 }
 
 void loop()
 {
     unsigned long currentMillis = millis();
+    // delay(50);
+    Serial.print("Ping: ");
+    // Serial.print(sonar_l.ping_cm());
+    Serial.print(sonar_l.ping_median(10) / US_ROUNDTRIP_CM);
+    Serial.print("cm, Time Taken: ");
+    Serial.print((millis() - currentMillis)/1000.0);
+    Serial.println("s");
+    
+    // Check distance in sensor 1
+    //   if it's close
+    //     sound alarm depending on distance
+    // check sensor 2
+    //  if close
+    //      sound alarm depending on distance
     
 }
+
